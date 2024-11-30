80686ecf746e0b2bee67260f152d79e9cc0ba8f7
AhmedFared888 <ahmedfared3404814@gmai.com>
2024-11-30 02:21:19 -0800

    adding custom app bar

---
+++ lib/widgets/custom_search_icon.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 45,
+      height: 45,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white.withOpacity(.05),
+      ),
+      child: const Center(
+        child: Icon(
+          Icons.search,
+          size: 25.0,
+        ),
+      ),
+    );
+  }
+}
