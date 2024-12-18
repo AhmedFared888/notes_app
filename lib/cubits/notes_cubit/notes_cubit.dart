327c3c674c065dc3ef203581b1f7eb49236a64b2
AhmedFared888 <ahmedfared3404814@gmai.com>
2024-12-17 19:44:10 -0800

    display notes part one

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -4,17 +4,14 @@ import 'package:meta/meta.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
-part 'notes_cubit_state.dart';
+part 'notes_state.dart';
 
-class NotesCubit extends Cubit<NotesCubitState> {
+class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesCubitInitial());
+  List<NoteModel>? notes;
 
   fetchAllNotes() async {
-    try {
-      var notesBox = Hive.box<NoteModel>(kNoteBox);
-      emit(NotesCubitSuccess(notesBox.values.toList()));
-    } catch (e) {
-      emit(NotesCubitFailure(e.toString()));
-    }
+    var notesBox = Hive.box<NoteModel>(kNoteBox);
+    notes = notesBox.values.toList();
   }
 }
